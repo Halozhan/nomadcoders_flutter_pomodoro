7e29ab231f38027302d023b203033fd31d684fa6
Halozhan <juwonhwang0411@gmail.com>
2024-07-22 22:17:41 +0900

    feat: Add home screen with Pomodoro timer functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:nomadcoders_flutter_pomodoro/screen/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFE7626C),
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
+      home: const HomeScreen(),
     );
   }
 }
